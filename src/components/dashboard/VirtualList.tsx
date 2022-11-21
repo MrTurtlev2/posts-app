88c31f5c210c76b37957e3692117ef8144e7eaa9
Marek <wojnarmarek0@gmail.com>
2022-11-21 20:36:20 +0100

    virtual list refactor

--- src/components/dashboard/VirtualList.tsx
+++ src/components/dashboard/VirtualList.tsx
@@ -1,8 +1,53 @@
 import React from "react";
+import AutoSizer from "react-virtualized-auto-sizer";
+import {FixedSizeList as List} from "react-window";
+import {useAppSelector} from "../../types/hooks";
+import styled from "styled-components";
+
+
+const rowRenderer = ({key, index, style}: any) => {
+    return (
+        <PostTile key={key} style={style}>
+            row
+            {/*<p>{JSON.stringify(photosArr)}</p>*/}
+            {/*<p>{JSON.stringify(photosArr)}</p>*/}
+            {/*<PostTile {...photosArr[index]} />*/}
+        </PostTile>
+    );
+}
 
 const VirtualList = () => {
+    const photosArr = useAppSelector(state => state.getPhotos.photos);
+
     return (
-        <p>rrfrf</p>
+        <VirtualListWrap>
+            <AutoSizer>
+                {({height, width}: any) => (
+                    <List
+                        height={height}
+                        itemCount={photosArr.length}
+                        itemSize={50}
+                        width={width}
+                    >
+                        {rowRenderer}
+                    </List>
+                )}
+            </AutoSizer>
+        </VirtualListWrap>
     )
 }
 export default VirtualList
+
+
+const VirtualListWrap = styled.div`
+  background-color: ${({theme}) => theme.white};
+  height: 90vh;
+`;
+
+const PostTile = styled.div`
+  background-color: wheat;
+
+  &:hover {
+    background-color: red;
+  }
+`;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,7 @@
 import React, {useEffect} from "react";
 import {useAppDispatch, useAppSelector} from "../types/hooks";
 import {getPhotosAsync} from "../state/photos/GetPhotosSlice";
-import {FixedSizeList as List} from "react-window";
-import AutoSizer from "react-virtualized-auto-sizer";
-import styled from "styled-components";
+import VirtualList from "../components/dashboard/VirtualList";
 
 const Dashboard = () => {
 
@@ -14,45 +12,10 @@ const Dashboard = () => {
         dispatch(getPhotosAsync());
     }, [dispatch])
 
-    // console.log(photosArr)
-
-    const rowRenderer = ({key, index, style}: any) => {
-        return (
-            <div key={key} style={style}>
-                row
-                {/*<p>{JSON.stringify(photosArr)}</p>*/}
-                {/*<p>{JSON.stringify(photosArr)}</p>*/}
-                {/*<PostTile {...photosArr[index]} />*/}
-            </div>
-        );
-    }
 
     return (
-        // <div>
-        //     {photosArr.map((post: any, index) => {
-        //         return <PostTile key={index} {...post} />
-        //     })}
-        // </div>
-        <VirtualListWrap>
-            <AutoSizer>
-                {({height, width}: any) => (
-                    <List
-                        height={height}
-                        itemCount={photosArr.length}
-                        itemSize={50}
-                        width={width}
-                    >
-                        {rowRenderer}
-                    </List>
-                )}
-            </AutoSizer>
-        </VirtualListWrap>
 
+        <VirtualList/>
     )
 }
 export default Dashboard
-
-const VirtualListWrap = styled.div`
-  background-color: ${({theme}) => theme.white};
-  height: 90vh;
-`;
